9117280580dd253d0194b18458eda6af15fd59d7
ka1em <wangkaiminchn@gmail.com>
2016-09-27 16:32:23 +0800

    test mutex

--- t3/t34.go
+++ t3/t34.go
@@ -2,21 +2,26 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
 )
 
 func main() {
+	runtime.GOMAXPROCS(4)
 	current := 0
 	n := 100
 	wg := new(sync.WaitGroup)
+	mt := new(sync.Mutex)
 
 	for i := 0; i < n; i++ {
 		wg.Add(1)
 		go func() {
+			mt.Lock()
 			current++
+			mt.Unlock()
 			fmt.Println(current)
 			wg.Done()
 		}()
-		wg.Wait()
 	}
+	wg.Wait()
 }
